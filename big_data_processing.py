63286fc57a4233359b4e3c07bebaac43ec01062b
taparnat <132565548+taparnat@users.noreply.github.com>
2024-05-16 18:38:37 +0200

    Update big_data_processing.py

--- big_data_processing.py
+++ big_data_processing.py
@@ -1,9 +1,17 @@
-#codes for processing big data in agriculture
+#codes for processing big data in agriculture from MOOC 
 #download some type of data for studying which has a huuuge size 
 import opencv #for 3D images
 import matplotlib.pyplot as plt #for plotting 
 from nicegui import ui #for frontend UI 
 import pandas #for datafraame
 import numpy #for arrays 
-#map function in blocks of big data: synatx - map(function, collection
+#map function in blocks of big data: synatx - map(function, collection)
 from math import sqrt
+roots = map(sqrt, (2,3,4,5,6,7)
+print (roots)
+#when there are more collections, map still works the same map(function, collection_1, collection_2)
+from math import mean
+means = map(mean, (2,3,4), (8,9,2), (5, 2, 9))
+
+
+            
